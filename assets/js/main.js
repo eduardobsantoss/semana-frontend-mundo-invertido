059051ab85a0587c06567b99adca755044ab7985
eduardobsantoss <barbosa.eduardosan@gmail.com>
2024-12-14 14:18:22 -0300

    "Primeiro commit"
    Necessário algumas alterações pois está dando um conflito de permissão de dados com algumas imagens disponibilizadas.

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+import { getHellfireClubSubscriptions, subscribeToHellfireClub } from "hellfire-club.js";
+
+    const txtName = document.getElementById('txtName')
+    const txtEmail = document.getElementById('txtEmail')
+    const txtLevel = document.getElementById('txtLevel')
+    const txtCharacter = document.getElementById('txtCharacter')
+
+    const btnSubscribe = document.getElementById('btnSubscribe')
+
+    btnSubscribe.addEventListener('click', async () => {
+        const subscription = {
+            name: txtName.value,
+            email: txtEmail.value,
+            level: txtLevel.value,
+            character: txtCharacter.value
+        }
+        const subscriptionId = await subscribeToHellfireClub(subscription)
+      
+        
+         txtName.value = ''
+         txtEmail.value = ''
+         txtLevel.value = ''
+         txtCharacter.value = ''
+
+         alert(`Inscrito com sucesso: ${subscriptionId}`)
+
+    })
+
+    async function loadData(){
+        const subscription = await getHellfireClubSubscriptions();
+        console.log(subscription)
+    }
+
+    loadData();
